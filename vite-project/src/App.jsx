152939ab47b5333de6f46405ca769ab59aa35d1b
David Sanchez <davidendubai@gmail.com>
2024-10-21 20:48:09 -0500

    Finalizacion form

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,6 +6,8 @@ import './App.css'
 function App() {
   const [usuario, setUsuario] = useState ('')
   const [clave, setClave] = useState ('')
+  const [textoAvoz, setTextoAvoz] = useState ('')
+  const [vozATexto, setVozATexto] = useState ('')
 
   function cambiarUsuario(evento) {
     setUsuario(evento.target.value)
@@ -14,13 +16,32 @@ function App() {
   function cambiarClave(evento) {
     setClave(evento.target.value)
     
-
   }
   function ingresar() {
     if (usuario == 'admin' && clave == 'admin'){
       alert('Ingresaste')
     }else{
       alert('Usuario o clave incorrecto')
+    }
+  }
+
+  function cambiarTexto(evento) {
+    setTextoAvoz(evento.target.value)
+    
+  }
+
+  function convertirTextoAvoz() {
+    const synth = window.speechSynthesis
+    const utterThis = new SpeechSynthesisUtterance(textoAvoz)
+    synth.speak(utterThis) 
+  }
+
+  function grabarVozATexto() {
+    const recognition = new window.webkitSpeechRecognition()
+    recognition.lang = 'es-ES'
+    recognition.start()
+    recognition.onresult = function(event) {
+      setVozATexto(event.results[0][0].transcript)
     }
     
   }
@@ -29,6 +50,15 @@ function App() {
     <input type="text" name="usuario" id="usuario" value={usuario} onChange={cambiarUsuario}/> 
     <input type="password" name="clave" id="clave" value={clave} onChange={cambiarClave} />
     <button onClick={ingresar}>Ingresar</button>
+
+    <br />
+    <h3>Conversor de texto a voz</h3>
+    <input type="text" name="" id="textoAvoz" value={textoAvoz} onChange={cambiarTexto}/>
+    <button onClick={convertirTextoAvoz}>Convertir</button>
+
+    <h3>Conversor de voz a texto</h3>
+    <button onClick={grabarVozATexto}>Grabar</button>
+    {vozATexto}
    
     </>
   )
